cdda546eb8c9d28b98d8192b2af572e29eba5454
rachitpoudel101 <rachit300poudel@gmail.com>
2024-12-07 21:27:27 +0545

    add the restframework on dajngo files

--- Backend/Backend_auth/urls.py
+++ Backend/Backend_auth/urls.py
@@ -15,8 +15,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/obtain/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+# path('token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+    path('api/user/register/', CreateUserView.as_view(), name='register'),
+    path('api-auth',include("rest_framework.urls")),
 ]
--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework import generic
+from .serializers import UserSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
-# Create your views here.
+class CreateUserView(generic.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = User
+    permission_classes = [AllowAny]
