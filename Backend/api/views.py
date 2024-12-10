20276842ae3e85a152df8c683c71567dce8cf194
rachitpoudel101 <rachit300poudel@gmail.com>
2024-12-10 20:34:01 +0545

    add the mote serializer and on views add delete list the notes.

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,15 +1,36 @@
-from rest_framework.generics import CreateAPIView, ListAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.shortcuts import render
 from django.contrib.auth.models import User
-from .serializers import UserSerializer
+from rest_framework import generics
+from .serializers import UserSerializer, NoteSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .models import Note
 
-# Register a new user
-class CreateUserView(CreateAPIView):
-    serializer_class = UserSerializer
-    permission_classes = [AllowAny]
 
-# List all users
-class ListUsersView(ListAPIView):
+class NoteListCreate(generics.ListCreateAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            serializer.save(author=self.request.user)
+        else:
+            print(serializer.errors)
+
+
+class NoteDelete(generics.DestroyAPIView):
+    serializer_class = NoteSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return Note.objects.filter(author=user)
+
+
+class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
