3cc7cd6c01daae023ab6da90e49446b37053ad71
rachitpoudel101 <rachit300poudel@gmail.com>
2025-02-26 20:46:23 +0545

    update the path of the landing page

--- Backend/user/urls.py
+++ Backend/user/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.signup, name='signup'),
-    path('login/', views.user_login, name='login'),
+    path('signup', views.signup, name='signup'),
+    path('', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),  # Update this line
     path('dashboard/', views.dashboard, name='dashboard'),
     path('admin-dashboard/', views.admin_dashboard, name='admin_dashboard'),
