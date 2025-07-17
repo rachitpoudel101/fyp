e2f5f1a335492fc96aa43b8290296d4040e47d56
rachitpoudel101 <rachit300poudel@gmail.com>
2025-07-17 12:38:59 +0545

    Add the rest framke work

--- inventory_man_system/settings.py
+++ inventory_man_system/settings.py
@@ -50,9 +50,13 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'rest_framework_simplejwt',
     "user",
     "Inventory",
     "widget_tweaks",
+    'app',
+    
 ]
 
 MIDDLEWARE = [
