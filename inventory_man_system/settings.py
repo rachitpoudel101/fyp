d9a9f3018e5fa95285b1a0c0f83910e353369e46
rachitpoudel101 <rachit300poudel@gmail.com>
2025-07-20 11:28:24 +0545

    Upodate in hte code .

--- inventory_man_system/settings.py
+++ inventory_man_system/settings.py
@@ -95,12 +95,12 @@ WSGI_APPLICATION = "inventory_man_system.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+    # DATABASES = {
+    #     "default": {
+    #         "ENGINE": "django.db.backends.sqlite3",
+    #         "NAME": BASE_DIR / "db.sqlite3",
+    #     }
+    # }
 
 DATABASES = {
     "default": dj_database_url.parse(
